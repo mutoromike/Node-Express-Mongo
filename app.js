443c1ef23ab957a5ec7b97479b7a6c9efcd6afbb
strongestavenger <mikemutoro@gmail.com>
2019-08-03 14:09:03 +0300

    add models - product model

--- app.js
+++ app.js
@@ -4,3 +4,9 @@ const bodyParser = require('body-parser');
 
 // initialize our express app
 const app = express();
+
+let port = 1234;
+
+app.listen(port, () => {
+    console.log('Server is up and running on port number ' + port);
+});

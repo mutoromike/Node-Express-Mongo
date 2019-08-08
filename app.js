bb1f939e7db80fde9a93dc008a092651b91e58e7
strongestavenger <mikemutoro@gmail.com>
2019-08-08 16:02:55 +0300

    initialize functionality

--- app.js
+++ app.js
@@ -1,11 +1,37 @@
 // app.js
 const express = require('express');
 const bodyParser = require('body-parser');
+// const MongoClient = require('mongodb').MongoClient;
+const mongoose = require('mongoose');
 
-// initialize our express app
+const product = require('./routes/product.routes');
 const app = express();
+// const url = "mongodb://127.0.0.1" ;
 
-let port = 1234;
+// Database Name
+const dbName = 'starter';
+
+// Use connect method to connect to the server
+// MongoClient.connect(url, function(err, client) {
+//     try {
+//         console.log("Connected successfully to server");
+//         const db = client.db(dbName);
+//         client.close();
+//     } catch (err) {
+//         console.log("the error is", err);
+//     }
+  
+// });
+mongoose.connect('mongodb://localhost:27017/' + dbName)
+
+
+const port = 5000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use('/products', product);
+
+
 
 app.listen(port, () => {
     console.log('Server is up and running on port number ' + port);
--- routes/product.routes.js
+++ routes/product.routes.js
@@ -7,4 +7,5 @@ const product_controller = require('../controllers/product.controller');
 
 // a simple test url to check that all of our files are communicating correctly.
 router.get('/test', product_controller.test);
+router.post('/create', product_controller.product_create);
 module.exports = router;
